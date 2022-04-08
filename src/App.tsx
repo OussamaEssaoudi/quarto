2e105e8b4c7d5430c9a1f59d8ed63472a6abc0fc
oussama-ideri <oussamajallali6@gmail.com>
2022-04-08 03:52:38 +0200

    Update tree structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routes from "./Utils/routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {routes.map((route) => (
+          <Route
+            key={route.name}
+            path={route.path}
+            element={<route.element />}
+          />
+        ))}
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/Utils/routes.ts
@@ -0,0 +1,20 @@
+type routesType = {
+  name: string;
+  path: string;
+  element: any;
+}[];
+
+const routes: routesType = [
+  /* {
+    name: "Homepage",
+    path: "/",
+    element: HomePage,
+  },
+  {
+    name: "Game",
+    path: "/game",
+    element: Game,
+  } */
+];
+
+export default routes;
